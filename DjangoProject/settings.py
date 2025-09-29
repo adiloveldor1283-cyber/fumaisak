6453225dbb28a6088a5bf93e0c74ec0937b858ca
adiloveldor1283-cyber <adiloveldor1283@gmai.com>
2025-09-29 22:15:18 +0500

    first commit

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static')),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
